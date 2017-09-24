438bf76a66aa884b666fd7836a48b8f3d89195d6
arorayash <arorayash.thisisus@gmail.com>
2017-09-25 00:00:37 +0530

    lazy load first attempt

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 import ProgressiveLoader from "./Components/ProgressiveLoader";
 
+const imageURL = 'https://dl.dropboxusercontent.com/s/ibv5i4uj50o9o2l/mainImage.jpg?dl=1';
+const placeholderHeight = "50vw";
+const placeholderWidth = "50vh";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ProgressiveLoader></ProgressiveLoader>
+      <div className="app">
+        <div className="app-iron-image-container">
+          <ProgressiveLoader placeholderHeight={placeholderHeight} placeholderWidth={placeholderWidth} srcLoaded={imageURL} />
+        </div>
       </div>
     );
   }
--- src/Components/ProgressiveLoader.js
+++ src/Components/ProgressiveLoader.js
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import './ProgressiveLoader.css';
 
 export default class ProgressiveLoader extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading : true
+        }
+
+        this.mainImage = null;
+    }
+
+    componentDidMount() {
+        const mainLoaderImg = new Image();
+        mainLoaderImg.src = this.props.srcLoaded;
+        mainLoaderImg.onload = () => {
+            this.mainImage.setAttribute(
+                'style',
+                `background-image: url('${this.props.srcLoaded}')`
+            );
+            this.mainImage.classList.add('main-image-fade-in');
+            this.setState({loaded:false});
+        }
+    };
+
     render() {
+        const loaderStyle = this.state.loading ? {display : "block"}:{display:"none"};
         return (
-            <div className="App">
-                Progressive
+            <div className="main-image-container">
+                <div className="main-image-loaded"
+                    ref={imageLoadedElem => this.mainImage = imageLoadedElem}>
+                </div>
+                <div style={loaderStyle} className="preload-animated-background">
+                </div>
             </div>
-        );
+        )
     }
 }
