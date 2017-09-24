754e2fd09ec737bf7fdcfd0e373cd9a4526494af
arorayash <arorayash.thisisus@gmail.com>
2017-09-25 00:04:28 +0530

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 ## Progressive-loader component
+
+Setup guide
+```$xslt
+cd <project folder>
+npm install
+npm start
+```
+
+The project is under process. The progressive image loader component has to be provided with the image-resource URL.
--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import './App.css';
 import ProgressiveLoader from "./Components/ProgressiveLoader";
 
 const imageURL = 'https://dl.dropboxusercontent.com/s/ibv5i4uj50o9o2l/mainImage.jpg?dl=1';
-const placeholderHeight = "50vw";
-const placeholderWidth = "50vh";
+
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <div className="app-iron-image-container">
-          <ProgressiveLoader placeholderHeight={placeholderHeight} placeholderWidth={placeholderWidth} srcLoaded={imageURL} />
+          <ProgressiveLoader srcLoaded={imageURL} />
         </div>
       </div>
     );
